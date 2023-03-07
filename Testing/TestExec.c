c10c1282b877a46d551bb69257501a30499952a3
Bruno Spoletini <39472059+BrunoSpoletini@users.noreply.github.com>
2023-03-07 16:02:04 -0300

    New names
    no more

--- Testing/TestExec.c
+++ Testing/TestExec.c
@@ -4,10 +4,7 @@ int main(){
 
     printf("Hola soy A y tengo PID = %d\n",getpid() );
 
-    execl("./B",NULL);
-
-    //printf("Hola soy ls y tengo PID = %d\n",getpid() );
-
+    execl("./a", (char*) NULL);
 
     return 0;
 }
