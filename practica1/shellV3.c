d3a5f6b167f0bf7a1361b5a99b92869937c67834
BrunoSpoletini <bruno_spoletini@hotmail.com>
2023-03-24 14:14:18 -0300

    Piping finally implemented in shellV3

--- practica1/shellV3.c
+++ practica1/shellV3.c
@@ -16,8 +16,9 @@ directorio donde se encuentre lo que queremos ejecutar.
 
 
 TO DO:
-De momento, soporta un unico redireccionamiento de saldia estandar a la vez.
 Checkeo de error en el malloc?
+Checkeo de fallo de exec
+Checkeo de fallo de dup
 */
 void freeArr(char **args){
     int i = 0;
@@ -28,33 +29,21 @@ void freeArr(char **args){
 }
 
 void runP(char** args){
-    int err = 0, tempFD0, tempFD1, fileFD, red = 0, i = 0, procesos = 0, pos[256], pipeFD[2], cont=0;
-    char **args1, **filename, simb, *proc[256][32];
+    int err = 0, tempFD0, tempFD1, fileFD, red = 0, i = 0, procesos = 0, pipeFD[2], cont=0;
+    char **filename, *proc[256][32];
     pid_t pid;
 
-
     while( (args[i] != NULL) && (red == 0) ){
-        /*if ( strcmp(args[i], ">" ) == 0 ){
+        if ( strcmp(args[i], ">" ) == 0 ){
             filename = &args[i+1];
-            args[i] = NULL;
+            proc[procesos][cont]=NULL;
             fileFD = open(filename[0], O_RDWR | O_CREAT | O_APPEND, 0666);
             dup2(fileFD, 1);
             red = 1;    
-        }*/
-        // if ( i == 0 ) {
-        //     pos[procesos] = i;
-        //     procesos++;
-        // }
-
-        
-
-        if ( strcmp(args[i], "|") == 0 ){
+        } else if ( strcmp(args[i], "|") == 0 ){
+            proc[procesos][cont]=NULL;
             procesos++;
             cont = 0;
-            // pos[procesos] = i+1;
-            // procesos++;
-            // args[i] = NULL;
-            
         } else {
             proc[procesos][cont] = malloc(sizeof(args[i]));
             strcpy (proc[procesos][cont] , args[i]);
@@ -62,6 +51,7 @@ void runP(char** args){
         }
         i++;
     }
+    proc[procesos][cont]=NULL; //Para que no tome los argumentos que quedan buffereados
     procesos++;
 
     if (procesos == 1){
@@ -69,15 +59,8 @@ void runP(char** args){
     } else {
         tempFD0 = dup(0);
         tempFD1 = dup(1);
-
-        
-        for (int j = 0; j < procesos; j++){
-            
+        for(int j=0; j < procesos ; j++){  
             pipe(pipeFD);
-
-            int fdin = j == 0 ? tempFD0 : pipeFD[0] ;
-            int fdout = j == (procesos -1) ? tempFD1 : pipeFD[1];
-
             pid = fork();
             switch (pid) {
                 case -1:
@@ -85,37 +68,30 @@ void runP(char** args){
                     exit(0);
                     break;
                 case 0: //Child
-                    close(pipeFD[1]);
-                    //dup2(fdin, 0);
-                    dup2(fdin, 0);
-
+                    close(pipeFD[0]);
+                    //El proceso de la derecha de todo escribe por la salida estandar
+                    if (j != (procesos-1)){
+                        dup2(pipeFD[1], 1);
+                    } else {
+                        dup2(tempFD1, 1);//Solo por claridad de codigo
+                    }
                     execvp(proc[j][0], proc[j]);
                     break;
                 default: //Parent
-
-                    dup2(fdout,0);
-                     if (j > 0) {
-                        close(pipeFD[0]);
-                    }
-                    //wait(NULL);
+                    close(pipeFD[1]);
+                    dup2(pipeFD[0], 0);
                     break;
             }
         }
         dup2(tempFD0,0);
-
+        dup2(tempFD1,1);
     }
 
-
-
-    // err = execvp(args[0], args); //Busca la funcion "fun" en el path y la ejecuta con los argumentos args[]
     // if (err != 0) {
     //     printf("Orden no encontrada\n");
     //     err = 0;
     // }
-
 }
-
-
 
 int main(){
     int index = 0, err = 0;
@@ -148,8 +124,6 @@ int main(){
                 break;
         }
     }
-
     free(args);
-    
     return 0;
 }
